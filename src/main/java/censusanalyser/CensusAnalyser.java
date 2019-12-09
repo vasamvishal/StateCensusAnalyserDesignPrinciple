261ae68e4ad211fbb146797d5a1c531b016a861b
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-12-09 13:27:24 +0530

    REFACTOR:code and programmed for interface to adress open close principle

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -12,39 +12,33 @@ import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
-            Iterator<IndiaStateCodeCSV> censusCSVIterator =this.getCSVFileIterator(reader,IndiaCensusCSV.class);
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.
+                    getCSVFileIterator(reader, IndiaCensusCSV.class);
             return this.getCount(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
+
     public int loadIndiaStateCode(String indiaStateCodeFilePath) throws CensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(indiaStateCodeFilePath))){
-            Iterator<IndiaStateCodeCSV> censusCSVIterator =this.getCSVFileIterator(reader,IndiaStateCodeCSV.class);
+        try (Reader reader = Files.newBufferedReader(Paths.get(indiaStateCodeFilePath))) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvBuilder
+                    .getCSVFileIterator(reader, IndiaStateCodeCSV.class);
             return this.getCount(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
-    private <E> int getCount(Iterator<E> censusCSVIterator){
-        Iterable<E> csvIterable = ()-> censusCSVIterator;
-        int numOfEateries=(int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+
+    private <E> int getCount(Iterator<E> censusCSVIterator) {
+        Iterable<E> csvIterable = () -> censusCSVIterator;
+        int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
         return numOfEateries;
-    }
-    private <E> Iterator<E> getCSVFileIterator(Reader reader,Class csvClass) throws CensusAnalyserException {
-        try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return  csvToBean.iterator();
-        } catch (IllegalStateException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
-        }
     }
 }
 
--- src/main/java/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -1,4 +1,22 @@
 package censusanalyser;
 
-public class OpenCSVBuilder {
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSVBuilder<E> implements ICSVBuilder {
+    public  Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return  csvToBean.iterator();
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
 }

d6ae6b2768b8b32466d3c70bf4001836f2a13986
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-12-13 12:24:24 +0530

    ADD:Code for testcase for handling two us file

--- src/test/java/censusanalyser/USAdapterTest.java
+++ src/test/java/censusanalyser/USAdapterTest.java
@@ -55,4 +55,14 @@ public class USAdapterTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.HEADER_EXCEPTION,e.type);
         }
     }
+    @Test
+    public void givenTwoUsfile_ShouldReturnException() {
+        try {
+            USCensusAdaptor usCensusAdaptor = new USCensusAdaptor();
+            Map<String, CensussDAO> censusData = usCensusAdaptor.loadCensusData(CensusAnalyser.COUNTRY.US, US_CENSUS_FILE_PATH, INDIA_STATE_CODE_FILE_PATH);
+            Assert.assertEquals(51, censusData.size());
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.ARRAY_OUT_OF_BOUND, e.type);
+        }
+    }
 }
